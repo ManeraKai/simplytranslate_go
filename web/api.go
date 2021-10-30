0316ad346a7bdd00e86686799fdb11c1f3e7739b
ManeraKai <manerakai@protonmail.com>
2021-10-30 14:33:14 +0300

    added tts

---
+++ web/api.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/manerakai/simplytranslate_go/engines"
+)
+
+func translate(w http.ResponseWriter, r *http.Request) {
+
+	text := r.FormValue("text")
+	from := r.FormValue("from")
+	to := r.FormValue("to")
+
+	if text == "" {
+		writeError(w, 400, "'text' field is missing")
+	}
+	if from == "" {
+		writeError(w, 400, "'from' field is missing")
+	}
+	if to == "" {
+		writeError(w, 400, "'to' field is missing")
+	}
+
+	fmt.Fprintf(w, engines.GoogleTranslateEngine.Translate(text, from, to))
+
+}
+
+func getSupportedLanguages(w http.ResponseWriter, r *http.Request) {
+	engine := r.FormValue("engine")
+
+	request, error := json.Marshal(engines.GetSupportedLanguages(engine))
+
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, string(request))
+
+}
+
+func tts(w http.ResponseWriter, r *http.Request) {
+
+	text := r.FormValue("text")
+	lang := r.FormValue("lang")
+
+	if text == "" {
+		writeError(w, 400, "'text' field is missing")
+	}
+	if lang == "" {
+		writeError(w, 400, "'from' field is missing")
+	}
+	if lang == "" || text == "" {
+		w.Header().Set("Content-Type", "application/html")
+		fmt.Fprint(w, "")
+	} else {
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Write(engines.GoogleTranslateEngine.TTS(text, lang))
+	}
+}
--- web/main.go
+++ web/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/manerakai/simplytranslate_go/engines"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -23,6 +20,8 @@ func index(w http.ResponseWriter, r *http.Request) {
 		}
 	}()
 
+	http.FileServer(http.Dir("/"))
+
 	fmt.Fprintf(w, string(b))
 }
 
@@ -31,44 +30,11 @@ func writeError(w http.ResponseWriter, status int, err interface{}) {
 	fmt.Fprintln(w, err)
 }
 
-func translate(w http.ResponseWriter, r *http.Request) {
-
-	text := r.FormValue("text")
-	from := r.FormValue("from")
-	to := r.FormValue("to")
-
-	if text == "" {
-		writeError(w, 400, "'text' field is missing")
-	}
-	if from == "" {
-		writeError(w, 400, "'from' field is missing")
-	}
-	if to == "" {
-		writeError(w, 400, "'to' field is missing")
-	}
-
-	fmt.Fprintf(w, engines.GoogleTranslateEngine.Translate(text, from, to))
-
-}
-
-func getSupportedLanguages(w http.ResponseWriter, r *http.Request) {
-	engine := r.FormValue("engine")
-
-	request, error := json.Marshal(engines.GetSupportedLanguages(engine))
-
-	if error != nil {
-		fmt.Println(error)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, string(request))
-
-}
-
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/api/translate", translate)
 	http.HandleFunc("/api/get_languages", getSupportedLanguages)
+	http.HandleFunc("/api/tts", tts)
 	fmt.Println("Running on http://localhost:3000/")
 	http.ListenAndServe(":3000", nil)
 }
