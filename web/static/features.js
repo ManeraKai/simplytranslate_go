1394adc9efa7cd5771b802d5e37d5781a9c08750
ManeraKai <manerakai@protonmail.com>
2021-11-16 19:15:43 +0300

    Tweaking the website

---
+++ web/static/features.js
@@ -0,0 +1,20 @@
+const textareas = document.getElementsByTagName("textarea")
+
+for (const i in textareas) {
+    if (Object.hasOwnProperty.call(textareas, i)) {
+        const textarea = textareas[i];
+        textarea.style.height = "";
+        textarea.style.height = textarea.scrollHeight + 10 + "px";
+        function resize() {
+            textarea.style.height = "";
+            textarea.style.height = textarea.scrollHeight + 10 + "px";
+        }
+        textarea.addEventListener("input", resize);
+        textarea.addEventListener("resize", resize)
+    }
+}
+document.onkeypress =
+    function (e) {
+        if (e.ctrlKey && (e.code == 'Enter' || e.code == 'NumpadEnter'))
+            document.getElementById('form').submit();
+    }
